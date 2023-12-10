38022a8fbe62ef633b856fc3068bde2f95e4523e
MacanitaDB <105833395+Mcanita@users.noreply.github.com>
2023-12-10 13:34:04 -0300

    Add files via upload

---
+++ Localizacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_INTEGRADOR
+{
+     public class Localizacion
+    {
+        private int posicionX;
+        private int posicionY;
+
+        public int PosicionX { get; set; }
+        public int PosicionY { get; set; }
+
+        public Localizacion(int horizontal, int vertical)
+        {
+            PosicionX = horizontal;
+            PosicionY = vertical;
+        }
+    }
+}
